3145de9a225841977343eb7a04b4a43781083f45
brianglw <brianglw0506@gmail.com>
2025-04-25 16:57:52 -0400

    feat[styled main page and write file, debugging edit and delete buttons]

--- index.js
+++ index.js
@@ -3,53 +3,48 @@ import bodyParser from 'body-parser';
 
 const app = express();
 const PORT = 3000;
-const posts = {list: []};
+let list = [];
 let editingIndex;
-//enable static files in ejs files
-app.use(express.static('public'));
 
-//enable body-parser to access body requests
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
-//post route for blog submissions 
-app.post('/submit', (req, res) => {
-    const data = {...req.body, date: new Date().toLocaleDateString(), id: posts.list.length };
-    posts.list.push(data);
-    console.log(data);
-    console.log(posts);
-    res.render("index.ejs", posts);
+//1. A post route for writing a new blog post
+    //Adds title, content, date, and id keys into an data object
+    //Pushes data into list and loop renders the entries into index.ejs
+app.post('/add', (req, res) => {
+    const data = {...req.body, date: new Date().toLocaleDateString(), id: list.length };
+    list.push(data);
+    res.render("index.ejs", {blog: list});
 })
 
-//set up routes for edit and delete requests
+//2. A post route for editing a specific post
+    //Request contains ID of the object, corresponding to its index 
+    //Conditionally renders the title and contents onto write.ejs with the property isEditing
+    //Delete the post with your removeEntry method, and hit up the /add route when user submits 
+    //Bug: edit should not remove your entry if you click on home page while in /write
 app.post('/edit', (req, res) => {
-    let id = req.body.edit;
-    let reqindex = posts.list.findIndex(item => item.id == id); 
-    console.log(id, posts.list.findIndex(item => item.id == id));
-    //render another route include a post request there to replace title and content and date values 
-    res.render("revise.ejs", {title: posts.list[reqindex].title, content: posts.list[reqindex].content});
-    editingIndex = posts.list[reqindex].id;
+    let editingId = req.body.id;
+    let editingIndex = list.findIndex(entry => entry.id == editingId); 
+    res.render("write.ejs", {title: list[editingIndex].title, content: list[editingIndex].content, editingId: editingId});
+    removeEntry(editingId);
+    refresh();
 })
 
-app.post('/', (req, res) =>{
-    console.log('remove route requested');
-    let id = req.body.remove;
-    remove(id);
-    res.render('index.ejs', posts);
+//3. A post route for deleting your entry
+    //Request contains ID of the object, which calls removeEntry(id) and renders the new posts
+    //Call on refreshID() to sort IDs, remember with a forEach() loop we iterate over the element properties
+app.post('/delete', (req, res) =>{
+    console.log('/delete route requested');
+    let deleteId = req.body.delete;
+    removeEntry(deleteId);
+    refresh();
+    res.render('index.ejs', {blog: list});
 })
 
-app.post('/revisions', (req, res) => {
-    const title = req.body.title;
-    const content = req.body.content;
-    const date = new Date().toLocaleDateString();
-    const data = {title: title, content: content, date: date, id: posts.list.length };
-    posts.list.push(data);
-    remove(editingIndex);
-    editingIndex = null;
-    res.render("index.ejs", posts);
-})
 //set up the home page
 app.get('/', (req, res) => {
-    res.render("index.ejs", posts);
+    res.render("index.ejs", {blog: list});
 });
 
 //sets up the write page
@@ -57,13 +52,20 @@ app.get('/write', (req, res) => {
     res.render("write.ejs");
 })
 
-function remove(id) {
-    posts.list = posts.list.filter((item) => {
-        if (item.id != id) {
-            return item;
+function refresh() {
+    list.map((entry, index) => {
+        entry.id = index;
+        return entry;
+    });
+    console.log(list);
+}
+function removeEntry(id) {
+    list = list.filter(entry => {
+        if (entry.id != id) {
+            return entry;
         }
     })
-    console.log(posts.list);
+    console.log(list);
 }
 
 //launches the server at port and notifies the user
